b3896386fa90e67955178f10d768a8ad0f035320
PhrotonX <cpascual185@gmail.com>
2022-07-23 17:40:26 +0800

    Include more code to generateUUID()

--- phpWebTools.php
+++ phpWebTools.php
@@ -10,7 +10,16 @@
 			
 		}
 		
-		public function generateUUID(){
+		public function generateUUID(int $variant, int $version){
+			
+		
+			$time_low = dechex(rand(0, 16));
+			$time_mid = "";
+			$time_hi_and_version = "";
+			$clock_seq_hi_and_res_clock_seq_low = "";
+			$node = "";
+			
+			
 			
 		}
 	}
