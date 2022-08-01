debd25d0904cd06fca8112d3e53157a1e7383c0c
PhrotonX <cpascual185@gmail.com>
2022-08-01 15:44:02 +0800

    Test 3: Move generatePrivateVariant() above generateUUID()
    Still causes an error.

--- phpWebTools.php
+++ phpWebTools.php
@@ -1,5 +1,5 @@
 <?php
-	$version = "Version 0.1.0.1 alpha test 2";
+	$version = "Version 0.1.0.1 alpha test 3";
 
 	class phpWebTools{
 	
@@ -11,6 +11,25 @@
 			
 		}*/
 		
+		private function generatePrivateVariant(int $variant){
+			//0 = Variant 10
+			//1 = Variant 110
+			//2 = Variant 0
+			//3 = Variant 111
+			switch($variant){
+				case 0:
+					return dechex(rand(8, 11));
+				case 1:
+					return dechex(rand(12, 13));
+				case 2:
+					return dechex(rand(0, 7));
+				case 3:
+					return dechex(rand(14, 15));
+				default:
+					return "Error";
+			}
+		}
+
 		public function generateUUID(int $version, int $variant){
 			
 			$time_low = array(dechex(rand(0, 15)), dechex(rand(0, 15)),
@@ -33,25 +52,7 @@
 			
 			return $time_low . "-" . $time_mid . "-" . $time_hi_and_version . "-" . $clock_seq_hi_and_res_clock_seq_low . "-" . $node;
 		}
-
-		private function generatePrivateVariant(int $variant){
-			//0 = Variant 10
-			//1 = Variant 110
-			//2 = Variant 0
-			//3 = Variant 111
-			switch($variant){
-				case 0:
-					return dechex(rand(8, 11));
-				case 1:
-					return dechex(rand(12, 13));
-				case 2:
-					return dechex(rand(0, 7));
-				case 3:
-					return dechex(rand(14, 15));
-				default:
-					return "Error";
-			}
-		}
+		
 	}
 
 ?>
