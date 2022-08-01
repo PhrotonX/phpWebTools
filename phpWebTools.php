7cf1d62c9e509bdcae0cecfb70c6fd594b0c232d
PhrotonX <cpascual185@gmail.com>
2022-08-01 15:41:51 +0800

    Test 2: Add more functionality to generateUUID()
    Include more functionality, with a crash on a new private funciton called generatePrivateUUID(). The crash was caused by undefined function.

--- phpWebTools.php
+++ phpWebTools.php
@@ -1,5 +1,5 @@
 <?php
-	$version = "Version 0.1.0.1 alpha test 1";
+	$version = "Version 0.1.0.1 alpha test 2";
 
 	class phpWebTools{
 	
@@ -11,17 +11,46 @@
 			
 		}*/
 		
-		public function generateUUID(int $variant, int $version){
+		public function generateUUID(int $version, int $variant){
 			
-		
-			$time_low = dechex(rand(0, 16));
-			$time_mid = "";
-			$time_hi_and_version = "";
-			$clock_seq_hi_and_res_clock_seq_low = "";
-			$node = "";
+			$time_low = array(dechex(rand(0, 15)), dechex(rand(0, 15)),
+						dechex(rand(0, 15)), dechex(rand(0, 15)),
+						dechex(rand(0, 15)), dechex(rand(0, 15)),
+						dechex(rand(0, 15)), dechex(rand(0, 15)));
+
+			$time_mid = array(dechex(rand(0, 15)), dechex(rand(0, 15)),
+						dechex(rand(0, 15)), dechex(rand(0, 15)));
+
+			$time_hi_and_version = array($version, dechex(rand(0, 15)),
+						dechex(rand(0, 15)), dechex(rand(0, 15)));
+
+			$clock_seq_hi_and_res_clock_seq_low = array(generatePrivateVariant($variant), dechex(rand(0, 15)),
+						dechex(rand(0, 15)), dechex(rand(0, 15)));
+
+			$node = array(dechex(rand(0, 15)), dechex(rand(0, 15)), dechex(rand(0, 15)), dechex(rand(0, 15)),
+						dechex(rand(0, 15)), dechex(rand(0, 15)), dechex(rand(0, 15)), dechex(rand(0, 15)),
+						dechex(rand(0, 15)), dechex(rand(0, 15)), dechex(rand(0, 15)), dechex(rand(0, 15)));
 			
-			return $time_low;
-			
+			return $time_low . "-" . $time_mid . "-" . $time_hi_and_version . "-" . $clock_seq_hi_and_res_clock_seq_low . "-" . $node;
+		}
+
+		private function generatePrivateVariant(int $variant){
+			//0 = Variant 10
+			//1 = Variant 110
+			//2 = Variant 0
+			//3 = Variant 111
+			switch($variant){
+				case 0:
+					return dechex(rand(8, 11));
+				case 1:
+					return dechex(rand(12, 13));
+				case 2:
+					return dechex(rand(0, 7));
+				case 3:
+					return dechex(rand(14, 15));
+				default:
+					return "Error";
+			}
 		}
 	}
 
